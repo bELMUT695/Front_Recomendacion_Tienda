77e4a260bb848a115af7388fb8b574cf3f53e901
bELMUT695 <antunezgonzalesv@gmail.com>
2022-01-03 23:14:43 -0500

    Cambio en la estrucuta v5

--- src/routes/app.js
+++ src/routes/app.js
@@ -2,9 +2,6 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from '../pages/login'
 import Home from '../pages/home';
-import menVendido from '../pages/menVendido';
-import menTodo from '../pages/menTodo';
-import menRecomendado from '../pages/menRecomendado';
 import ListWomen from '../pages/ListWomen';
 import Layout from '../container/Layout';
 //import Navbar from '../components/Navbar'
@@ -17,9 +14,7 @@ const App = () => (
           <Route exact path="/login" component={Login}/> 
           <Route exact path="/home" component={ Home }/> 
           <Layout>
-          <Route path="/menVendido" component={ menVendido } />
-          <Route path="/menTodo" component={ menTodo } />
-          <Route path="/menRecomendado" component={ menRecomendado } />
+         
           <Route exact path="/women-top-list" component={ ListWomen } />
           </Layout>
       </Switch> 

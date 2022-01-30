cc9207c5a27eca76b590a3b56167cd6004cef1be
bELMUT695 <antunezgonzalesv@gmail.com>
2022-01-29 23:34:40 -0500

    Cambios en el home

--- src/components/OrderItemDeatils.jsx
+++ src/components/OrderItemDeatils.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './styles/OrderDetailsWomen.scss'
-
+import React from "react";
+import "./styles/OrderDetailsWomen.scss";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
 import ReactStars from "react-rating-stars-component";
 
-const OrderItem=({ product })=>{
-
+const OrderItem = ({ product }) => {
   const options = {
     edit: true,
     color: "rgba(20, 20, 20, 0.1)",
@@ -14,34 +15,43 @@ const OrderItem=({ product })=>{
     isHalf: false,
   };
 
-
-  return(
-    <div className='container-order-details'>
-      <div className='content-order-item'>
-
-        <div className='content-iamgen-order'>
-          <img src={product[0].image[0].URL} alt="" />   
-          
-        </div>  
-
-        <div className='order-clothe-details'>
-          <span><b>{product[0].name}</b></span> <br /><br /><br />
-          
-          {
-            product[0].description
-          } <br /><br /><br />
-
-          <span>${product[0].unitPrice}</span> <br /><br /><br />
-
-          <button class="button-55" role="button">Agregar al carrito</button><br /><br /><br />
-          
-          
-          <ReactStars {...options} />{" "}
+  return (
+    <div className="container-order-details">
+      <div className="content-order-item">
+        <div className="content-iamgen-order">
+          <img src={product[0].image[0].URL} alt="" />
         </div>
 
-      </div> 
+        <div className="order-clothe-details">
+          <div className="titleDetails">
+            <div>{product[0].name}</div>
+            <label className="unitPrice">${product[0].unitPrice}</label>
+          </div>
+          <div className="titleDescritption">
+            <h1 className="product-description">Descripción</h1>
+            <div>{product[0].description}</div>
+          </div>
+          <button class="button-55" role="button">
+            Agregar al carrito
+          </button>
+          <div className="share-favorite">
+            <div className="share-and-favorite">
+              <FontAwesomeIcon icon={faHeart} className="img3" />
+              Favorito
+            </div>
+            <div className="share-and-favorite">
+              <FontAwesomeIcon icon={faShareAlt} className="img3" />
+              compartir
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <ReactStars {...options} />{" "}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default OrderItem;
